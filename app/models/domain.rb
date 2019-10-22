fd49218b315f0fd93fe14d59eb14768b0faa9311
Leonardo Fiório <leonardofiorio@id.uff.br>
2019-10-22 19:30:09 -0300

    Implementando validações personalizadas para os types dos models e ajustando as views

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,20 +1,33 @@
 class Domain < ApplicationRecord
 	
 	#Validações de atributos
-	validates_presence_of :name, :message => "deve ser informado!"
-	validates_presence_of :type_domain, :message => "deve ser informado!"
-	validates_presence_of :ttl, :message => "deve ser informado!"
-	validates_presence_of :primary_name_server, :message => "deve ser informado!"
-	validates_presence_of :contact, :message => "deve ser informado!"
-	validates_presence_of :refresh, :message => "deve ser informado!"
-	validates_presence_of :retry, :message => "deve ser informado!"
-	validates_presence_of :expire, :message => "deve ser informado!"
-	validates_presence_of :minimum, :message => " deve ser informado!"
+	validates_presence_of  :name, :message => "deve ser informado!"
+	validates_presence_of  :ttl, :message => "deve ser informado!"
+	validates_presence_of  :primary_name_server, :message => "deve ser informado!"
+	validates_presence_of  :contact, :message => "deve ser informado!"
+	validates_presence_of  :refresh, :message => "deve ser informado!"
+	validates_presence_of  :retry, :message => "deve ser informado!"
+	validates_presence_of  :expire, :message => "deve ser informado!"
+	validates_presence_of  :minimum, :message => " deve ser informado!"
+
+	# Validação customizada do type_domain
+	validate :type_domain_validate
 
 	# Relacionamento entre os domains: subdomains
 	belongs_to :domains, class_name: "Domain", optional:true, dependent: :destroy
 
 	# Relacionamento de Records pertencentes ao domain
 	has_many :records, class_name: "Record", :inverse_of => :domain 
+
+
+	# Método de validação do type_domain
+	def type_domain_validate
+		if self.type_domain.eql?('MASTER') or self.type_domain.eql?('SLAVE') or 
+			self.type_domain.eql?('FORWARD') or self.type_domain.eql?('STUB') or
+			self.type_domain.eql?('HINT')
+		else
+			errors.add(:type_domain, " invalido!")
+		end
+	end 
 
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,13 +1,24 @@
 class Record < ApplicationRecord
 
-	#attr_accessible: :host, :ttl, :content, :priority, :type_record, :domain
-
+	# Validações
 	validates_presence_of :host, :message => "não informado!"
 	validates_presence_of :ttl, :message => "não informado!"
 	validates_presence_of :content, :message => "não informado!"
 	validates_presence_of :priority, :message => "não informado!"
-	validates_presence_of :type_record, :message => "não informado!"
 	validates_presence_of :domain, :message => "não informado!"
 
+	#Validação personalizada para o type_record
+	validate :type_record_validate
+
+	# Validação do Domain a qual o Record pertence
 	belongs_to :domain, class_name: "Domain", optional: false
+
+	# Método de validação do type_record
+	def type_record_validate
+		if self.type_record.eql?('A') or self.type_record.eql?('TXT') or 
+			self.type_record.eql?('CNAME') or self.type_record.eql?('PTR')
+		else
+			errors.add(:type_record, " invalido!")
+		end
+	end 
 end
