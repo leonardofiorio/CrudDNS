4627abbb800c6c0c730e80fadf036842aa207af4
Leonardo Fiório <leonardofiorio@id.uff.br>
2019-10-22 21:15:36 -0300

    Inserindo primeira camada de validação IPV4

--- app/models/record.rb
+++ app/models/record.rb
@@ -10,6 +10,9 @@ class Record < ApplicationRecord
 	#Validação personalizada para o type_record
 	validate :type_record_validate
 
+	#Validação para caso do formato ipv4
+	validate :content_ipv4_validate
+
 	# Validação do Domain a qual o Record pertence
 	belongs_to :domain, class_name: "Domain", optional: false
 
@@ -21,4 +24,19 @@ class Record < ApplicationRecord
 			errors.add(:type_record, " invalido!")
 		end
 	end 
+
+	def content_ipv4_validate
+		if self.type_record.eql?('A')
+			if (ip_address?(self.content))
+			else
+				errors.add(:content, " formato ipv4 inválido!")
+			end
+		end	
+	end
+
+	# Retorna true caso o formato for valido e false para formato inválido
+	# Não restringe os valores de cada octeto
+	def ip_address?(str)
+  		!!(str =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
+	end
 end
